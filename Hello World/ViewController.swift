8fe13902771e221fbc7c57e8c8f317324dcca2d6
Thorvalld <anis.bejaoui@esprit.tn>
2020-09-23 12:41:23 +0100

    initializing project

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var x : Int?
+    let y = 100
+    var first_name : String?
+    var last_name : String = "Bob"
+    var age : String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
